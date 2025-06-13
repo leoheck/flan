936de7aba5e468fbd8797d5d981700e14c7f7ab5
Leandro Heck <1277920+leoheck@users.noreply.github.com>
2025-06-13 01:32:15 -0300

    Filter out alreary patched CVEs of openssh on Ubuntu

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 current_time=$(date "+%Y.%m.%d-%H.%M")
 if [[ -z $upload ]]
@@ -6,21 +6,21 @@ then
     root_dir=/shared/
 else
     root_dir=/
-    mkdir /xml_files
-    mkdir /reports
+    mkdir -p /xml_files
+    mkdir -p /reports
 fi
 
 report_extension="tex"
 
-if [[ ! -z $format ]]
+if [[ ! -z ${format} ]]
 then
-    report_extension=$format
+    report_extension=${format}
 fi
 
-xml_dir=xml_files/$current_time
-report_file=reports/report_$current_time.$report_extension
+xml_dir=xml_files/${current_time}
+report_file=reports/report_${current_time}.${report_extension}
 
-function upload {
+upload() {
     if [[ -z $upload ]]
     then
         return
@@ -33,25 +33,59 @@ function upload {
     fi
 }
 
-function get_filename(){
+get_filename(){
     echo $1 | tr / -
 }
 
-mkdir $root_dir$xml_dir
+filter_out_ubuntu_cvss(){
+
+    xmlfile="$1"
+    xmlfile_orig="${xmlfile}.orig"
+
+    # Backup generated XML for comparison...
+    cp ${xmlfile} ${xmlfile_orig}
+
+    if grep -qi "Ubuntu" ${xmlfile}; then
+
+        mkdir -p ${root_dir}/changelogs
+
+        openssh_version=$(xmlstarlet sel -t -v '//port/service[@name="ssh"]/@version' "${xmlfile}" | sed "s/ Ubuntu /-/g")
+        wget -q -O /shared/changelogs/openssh_${openssh_version} "https://changelogs.ubuntu.com/changelogs/pool/main/o/openssh/openssh_${openssh_version}/changelog"
+
+        patched_cvss=($(cat /shared/changelogs/openssh_${openssh_version} | grep -o 'CVE-[0-9]\{4\}-[0-9]\{4,7\}' | sort | uniq | tac))
+        cvss_found=($(grep -o 'CVE-[0-9]\{4\}-[0-9]\{4,7\}' ${xmlfile} | grep -o 'CVE-[0-9]\{4\}-[0-9]\{4,7\}' | sort | uniq | tac))
+
+        echo
+        echo "Verifying Ubuntu patches:"
+        for cve in ${cvss_found[@]}; do
+            echo -ne "- Checking ${cve}... \t"
+            if [[ " ${patched_cvss[*]} " == *" ${cve} "* ]]; then
+                echo "Already patched, filtering it out."
+                xmlstarlet ed -d "//table[elem[@key='id']='${cve}']" ${xmlfile} > /dev/null 2>&1
+            else
+                echo "Not fixed yet."
+            fi
+        done
+        echo
+    fi
+}
+
+mkdir -p ${root_dir}${xml_dir}
 while IFS= read -r line
 do
   current_time=$(date "+%Y.%m.%d-%H.%M.%S")
   filename=$(get_filename $line)".xml"
-  nmap -sV -oX $root_dir$xml_dir/$filename -oN - -v1 $@ --script=vulners/vulners.nse $line
-  upload $xml_dir/$filename
+  nmap -sV -oX ${root_dir}${xml_dir}/${filename} -oN - -v1 $@ --script=vulners/vulners.nse ${line}
+  filter_out_ubuntu_cvss ${root_dir}${xml_dir}/${filename}
+  upload ${xml_dir}/${filename}
 done < /shared/ips.txt
 
-python /output_report.py $root_dir$xml_dir $root_dir$report_file /shared/ips.txt
-if [[ $report_extension = "tex" ]]
+python /output_report.py ${root_dir}${xml_dir} ${root_dir}${report_file} /shared/ips.txt
+if [[ ${report_extension} = "tex" ]]
 then
-    sed -i 's/_/\\_/g' $root_dir$report_file
-    sed -i 's/\$/\\\$/g' $root_dir$report_file
-    sed -i 's/#/\\#/g' $root_dir$report_file
-    sed -i 's/%/\\%/g' $root_dir$report_file
+    sed -i 's/_/\\_/g' ${root_dir}${report_file}
+    sed -i 's/\$/\\\$/g' ${root_dir}${report_file}
+    sed -i 's/#/\\#/g' ${root_dir}${report_file}
+    sed -i 's/%/\\%/g' ${root_dir}${report_file}
 fi
-upload $report_file
+upload ${report_file}
